e298761aa54029eb9a01e026d7003cbcb7960f97
JunHyung <xk130@naver.com>
2023-08-05 19:32:00 +0900

    remove useless sign_up file

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask, jsonify, request, make_response, session
 from flask_login import LoginManager,current_user, login_required,login_user, logout_user
 from flask_cors import CORS
-from view import sign_up,login,student
+from view import login,student
 from model.mongodb import make_board_collection, make_course_collection, make_student_collection, make_subject_collection, make_teacher_collection
 #from blog_control.user_mgmt import User
 import os
@@ -21,7 +21,6 @@ make_subject_collection()
 #보안을 위해서는 서버를 끄고 켤때마다 다른값으로 해야하는데 그렇게하면 그동안 설정된 세션이 모두 사라진다.
 app.secret_key = 'dave_server3'#session 생성시 이 앱만의 secret key
 
-app.register_blueprint(sign_up.user_sign_up, url_prefix = '/signup')
 app.register_blueprint(login.user_login, url_prefix = '/api/login')
 app.register_blueprint(student.student, url_prefix = '/api/students')
 
