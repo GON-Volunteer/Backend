58533e14553b9057a916b9ec72ea4c433696f3f0
DESKTOP-9RTH6T9\user <tgool@naver.com>
2023-08-16 20:46:57 +0900

    가봅시다

--- app.py
+++ app.py
@@ -21,10 +21,17 @@ from model.mongodb import (
 # from blog_control.user_mgmt import User
 
 
-from view import login,student,access_check,teacher,pwchange,subject,course
+from view import login, student, access_check, teacher, pwchange, subject, course
 
-from model.mongodb import make_board_collection, make_course_collection, make_student_collection, make_subject_collection, make_teacher_collection
-#from blog_control.user_mgmt import User
+from model.mongodb import (
+    make_board_collection,
+    make_course_collection,
+    make_student_collection,
+    make_subject_collection,
+    make_teacher_collection,
+)
+
+# from blog_control.user_mgmt import User
 import os
 
 # https 만을 지원하는 기능을 http 에서 테스트할 때 필요한 설정
@@ -47,14 +54,13 @@ app.secret_key = "dave_server3"  # session 생성시 이 앱만의 secret key
 
 app.register_blueprint(board_mgmt.board, url_prefix="/api/articles")
 app.register_blueprint(comment_mgmt.comment, url_prefix="/api/comment")
-app.register_blueprint(login.user_login, url_prefix = '/api/login')
-app.register_blueprint(student.student, url_prefix = '/api/students')
-app.register_blueprint(access_check.access_check, url_prefix = '/api/auth')
-app.register_blueprint(subject.subject, url_prefix = '/api/subjects')
-app.register_blueprint(teacher.teacher, url_prefix = '/api/teachers')
-app.register_blueprint(course.course, url_prefix = '/api/courses')
-app.register_blueprint(pwchange.password_change, url_prefix = '/api/password')
-
+app.register_blueprint(login.user_login, url_prefix="/api/login")
+app.register_blueprint(student.student, url_prefix="/api/students")
+app.register_blueprint(access_check.access_check, url_prefix="/api/auth")
+app.register_blueprint(subject.subject, url_prefix="/api/subjects")
+app.register_blueprint(teacher.teacher, url_prefix="/api/teachers")
+app.register_blueprint(course.course, url_prefix="/api/courses")
+app.register_blueprint(pwchange.password_change, url_prefix="/api/password")
 
 
 @app.route("/")
@@ -62,22 +68,5 @@ def home():
     return "hello flask"
 
 
-
-@app.route("/api/login", methods=["POST"])
-def login():
-    new_user = request.get_json()
-    id = new_user["id"]
-    pw = new_user["password"]
-    return jsonify(
-        {
-            "_id": "admin",
-            "full_name": "najyong",
-            "account": "admin",
-            "code": "200",
-        }
-    )
-
-
-
-if __name__ == '__main__':
-    app.run(host = '127.0.0.1', port = 5000)
+if __name__ == "__main__":
+    app.run(host="127.0.0.1", port=5000)
--- view/login.py
+++ view/login.py
@@ -1,4 +1,13 @@
-from flask import Flask,Blueprint,request,make_response,jsonify,redirect,url_for,session
+from flask import (
+    Flask,
+    Blueprint,
+    request,
+    make_response,
+    jsonify,
+    redirect,
+    url_for,
+    session,
+)
 
 from flask_login import login_user, current_user, logout_user
 import datetime
@@ -13,34 +22,36 @@ secret_key = "gonitproject"
 user_login = Blueprint("login", __name__)  # login/login_teacher
 mongo_db = conn_mongodb()
 
-@user_login.route('/',methods = ['POST'])
+
+@user_login.route("/", methods=["POST"])
 def login():
     new_user = request.get_json()
-    id = new_user['id']
-    pw = new_user['password']
-    
-    row = mongo_db.student.find_one({'id':id})
-    
-    #pw.encode('UTF-8')은 유니코드 문자열인 PW를 UTF-8방식을 이용하여 바이트 문자열로 인코딩, row는 이미 바이트 문자열이다.
-    #들어온 입력의 id가 db에 있고 비밀번호가 맞으면 token발행
-    if row and bcrypt.checkpw(pw.encode('UTF-8'),row['hashed_pw']):
-        user_id = row['id'],
+    id = new_user["id"]
+    pw = new_user["password"]
+
+    row = mongo_db.student.find_one({"id": id})
+
+    # pw.encode('UTF-8')은 유니코드 문자열인 PW를 UTF-8방식을 이용하여 바이트 문자열로 인코딩, row는 이미 바이트 문자열이다.
+    # 들어온 입력의 id가 db에 있고 비밀번호가 맞으면 token발행
+    if row and bcrypt.checkpw(pw.encode("UTF-8"), row["hashed_pw"]):
+        user_id = (row["id"],)
         payload = {
-            'user_id' : user_id,
-            'account' : row['account'],
-            'exp' : datetime.utcnow()+timedelta(seconds = 60*60*24)#24시간 유효,UTC (협정 세계시)로 현재 날짜와 시간을 가져온다.
+            "user_id": user_id,
+            "account": row["account"],
+            "exp": datetime.utcnow()
+            + timedelta(seconds=60 * 60 * 24),  # 24시간 유효,UTC (협정 세계시)로 현재 날짜와 시간을 가져온다.
         }
-        token = jwt.encode(payload,secret_key,'HS256')
-        
-        return jsonify( 
+        token = jwt.encode(payload, secret_key, "HS256")
+
+        return jsonify(
             {
-                'code':"200",
-                 '_id': str(row['_id']),
-                'account' : row['account'],
-                'full_name': row['full_name'],
-                'access_token' : token
-        })
+                "code": "200",
+                "_id": str(row["_id"]),
+                "account": row["account"],
+                "full_name": row["full_name"],
+                "access_token": token,
+            }
+        )
 
     else:
-        return jsonify({"code":"400"})
-
+        return jsonify({"code": "400"})
