9ce3d0769b61f7db96972c6adfc25586450332d4
JunHyung <xk130@naver.com>
2023-08-27 00:19:40 +0900

    feat:: modify release student

--- view/assign_release.py
+++ view/assign_release.py
@@ -13,19 +13,26 @@ def assign_teacher_in_course():
     assign_teacher(assign_data['teachers'],assign_data['course_id'])
     return jsonify({"code":"200","message":"선생님 할당 완료"})
 
-@assign.route('/student',methods = ['POST'])
+@assign.route('/student',methods = ['POST','DELETE'])
 def assign_student_in_course():
-    assign_data = request.get_json()
-    assign_student(assign_data['students'],assign_data['course_id'])
-    return jsonify({"code":"200","message":"학생 할당 완료"})
-
+    if request.method == "POST":
+        assign_data = request.get_json()
+        assign_student(assign_data['students'],assign_data['course_id'])
+        return jsonify({"code":"200","message":"학생 할당 완료"})
+    else:
+        param_course_id = request.args.get('course-id')
+        param_student_id_list = request.args.getlist('student-id')
+        release_student(param_course_id,param_student_id_list)
+        
+        return jsonify({"code":"200","message":"학생 해제 완료"})
+        
 @assign.route('/teacher/<course_id>', methods = ['DELETE'])
 def delete_teacher_in_course(course_id):
     release_teacher(course_id)
     return jsonify({"code":"200","message":"선생님 해제 완료"})
 
-@assign.route('/students/<course_id>',methods = ['DELETE'])
-def delete_student_in_course(course_id):
-    release_student_id_list = request.get_json()['student_id']
-    release_student(course_id,release_student_id_list)
-    return jsonify({"code":"200","message":"학생 해제 완료"})
+# @assign.route('/students/<course_id>',methods = ['DELETE'])
+# def delete_student_in_course(course_id):
+#     release_student_id_list = request.get_json()['student_id']
+#     release_student(course_id,release_student_id_list)
+#     return jsonify({"code":"200","message":"학생 해제 완료"})
